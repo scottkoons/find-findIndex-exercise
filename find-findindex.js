3091ff35793a566b40e13f399ddc6bd023758a84
scottkoons <scottkoons@gmail.com>
2020-09-28 21:23:29 -0600

    Final

--- find-findindex.js
+++ find-findindex.js
@@ -11,9 +11,18 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function (name) {
+    return name.username === username;
+  });
+}
 
-function findUserByUsername(usersArray, username) {}
-
+const users = [
+  { username: "mlewis" },
+  { username: "akagen" },
+  { username: "msmith" },
+];
+// console.log(findUserByUsername(users, "taco"));
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
 
@@ -27,4 +36,14 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let foundIndex = usersArray.findIndex(function (name) {
+    return name.username === username;
+  });
+  if (foundIndex === -1) return;
+
+  return usersArray.splice(foundIndex, 1)[0];
+}
+
+console.log(removeUser(users, "akagen"));
+console.log(removeUser(users, "akagen"));
